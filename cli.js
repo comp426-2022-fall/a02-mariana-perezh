053e6832142599a9601162fe9290436b608514bb
mariana-perezh <mperezh@email.unc.edu>
2022-09-23 12:38:58 -0400

    can print help message now

--- cli.js
+++ cli.js
@@ -2,12 +2,12 @@
 
 const fs = require('fs');
 const minimist = require('minimist');
-const timezone = require('moment-timezone');
+const momentTimezone = require('moment-timezone');
 
 const args = minimist(process.argv.slice(2));
 
 if (args.h) {
-	const contents = readFileSync("help.txt", 'utf-8');
+	const contents = fs.readFileSync("help.txt", 'utf-8');
 	const helpText = contents.split(/\r?\n/);
 	console.log(helpText);
 	process.exit(0);
