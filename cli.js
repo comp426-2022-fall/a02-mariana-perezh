3d677a62880f6553f6cb1cee8fdac99604316668
mariana-perezh <mperezh@email.unc.edu>
2022-09-23 12:22:43 -0400

    not done

--- cli.js
+++ cli.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
+const fs = require('fs');
+const minimist = require('minimist');
+const timezone = require('moment-timezone');
+
+const args = minimist(process.argv.slice(2));
+
+
+
+const timezone = moment.tz.guest();
+
 
