cd920a969e4b3d46a6deab21a7e239b3326510ab
mariana-perezh <mperezh@email.unc.edu>
2022-09-23 12:33:27 -0400

    maybe printing out help text?

--- cli.js
+++ cli.js
@@ -6,7 +6,12 @@ const timezone = require('moment-timezone');
 
 const args = minimist(process.argv.slice(2));
 
-
+if (args.h) {
+	const contents = readFileSync("help.txt", 'utf-8');
+	const helpText = contents.split(/\r?\n/);
+	console.log(helpText);
+	process.exit(0);
+}
 
 const timezone = moment.tz.guest();
 
