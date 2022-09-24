a9cb921fe3383ae7c7ee3c4e5d3eb34a130be36a
mariana-perezh <mperezh@email.unc.edu>
2022-09-24 15:16:15 -0400

    added comments, think url works

--- cli.js
+++ cli.js
@@ -13,22 +13,28 @@ if (args.h) {
 	process.exit(0);
 }
 
-const timezone = moment.tz.guess();
+const timezone = momentTimezone.tz.guess();
 
 const latitude = args.n || args.s;
 const longitude = args.e || args.w;
 
 const day = args.d || 1;
 
+if (day == 0) {
+	console.log("today.");
+} else if (day > 1) {
+	console.log("in " + day + " days.");
+} else {
+	console.log("tomorrow.");
+}
+
 const echo = args.j;
 
-//should be finding the appropriate request url
 
-const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longitude=" + longitude + "&timezone" + timezone + "&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant" + "&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=" + day;
+const url = "https://api.open-meteo.com/v1/forecast?" + "latitude=" + latitude + "&longitude=" + longitude + "&timezone=" + timezone + "&daily=weathercode,temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_hours,windspeed_10m_max,winddirection_10m_dominant" + "&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York&past_days=" + day;
+
+// console.log(url);
 
 const response = await fetch(url);
 
 const data = awaite response.json();
-
-
-
